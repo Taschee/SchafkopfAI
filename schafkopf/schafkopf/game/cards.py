37198e371f2b8fd16bf7e0216def9d526d687eec
Alexander Sedlmayr <alexander.sedlmayr-ext@rio.cloud>
2019-04-26 17:01:52 +0200

    ranks

--- schafkopf/schafkopf/game/cards.py
+++ schafkopf/schafkopf/game/cards.py
@@ -1,7 +1,24 @@
+from schafkopf.schafkopf.game.ranks import ALL_RANKS
+from schafkopf.schafkopf.game.suits import ALL_SUITS
+
+
 class Card:
     def __init__(self, rank, suit):
+        if not self.is_legal(rank, suit):
+            raise InvalidCardDefinitionException("Invalid rank {} or suit {} for card definition".format(rank, suit))
         self.rank = rank
         self.suit = suit
 
     def beats(self, game_mode):
         return
+
+    @staticmethod
+    def is_legal(rank, suit):
+        if rank not in ALL_RANKS or suit not in ALL_SUITS:
+            return False
+        else:
+            return True
+
+
+class InvalidCardDefinitionException(Exception):
+    pass
--- schafkopf/schafkopf/game/ranks.py
+++ schafkopf/schafkopf/game/ranks.py
@@ -1,8 +1,52 @@
-SEVEN = 0
-EIGHT = 1
-NINE = 2
-TEN = 3
-UNTER = 4
-OBER = 5
-KING = 6
-ACE = 7
+class Rank:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        if self.value == 0:
+            return "Seven"
+        elif self.value == 1:
+            return "Eight"
+        elif self.value == 2:
+            return "Nine"
+        elif self.value == 3:
+            return "Unter"
+        elif self.value == 4:
+            return "Ober"
+        elif self.value == 5:
+            return "King"
+        elif self.value == 6:
+            return "Ten"
+        elif self.value == 7:
+            return "Ace"
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def points(self):
+        if self.value == 7:
+            return 11
+        elif self.value == 6:
+            return 10
+        elif self.value == 5:
+            return 4
+        elif self.value == 4:
+            return 3
+        elif self.value == 3:
+            return 2
+        else:
+            return 0
+
+
+SEVEN = Rank(0)
+EIGHT = Rank(1)
+NINE = Rank(2)
+UNTER = Rank(3)
+OBER = Rank(4)
+KING = Rank(5)
+TEN = Rank(6)
+ACE = Rank(7)
+ALL_RANKS = [SEVEN, EIGHT, NINE, TEN, UNTER, OBER, KING, ACE]
