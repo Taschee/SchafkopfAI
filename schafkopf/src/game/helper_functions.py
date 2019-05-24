c66b1025b31dfb0d894dcd23b50a74178692f7a0
Alexander Sedlmayr <alexander.sedlmayr-ext@rio.cloud>
2019-05-24 09:31:47 +0200

    sort hand

--- schafkopf/src/game/helper_functions.py
+++ schafkopf/src/game/helper_functions.py
@@ -1,8 +1,10 @@
+from typing import Optional, List
+
 from schafkopf.src.game.cards import Card
 from schafkopf.src.game.game_mode import GameMode
 from schafkopf.src.game.game_type import PARTNER_MODE, WENZ, SOLO
-from schafkopf.src.game.ranks import OBER, UNTER, ACE, NINE, EIGHT, TEN, KING, SEVEN
-from schafkopf.src.game.suits import ACORNS, HEARTS, LEAVES, BELLS
+from schafkopf.src.game.ranks import OBER, UNTER, ACE, NINE, EIGHT, TEN, KING, SEVEN, ALL_RANKS
+from schafkopf.src.game.suits import ACORNS, HEARTS, LEAVES, BELLS, ALL_SUITS
 
 
 def trump_cards(game_mode: GameMode):
@@ -18,3 +20,20 @@ def trump_cards(game_mode: GameMode):
                 Card(UNTER, ACORNS), Card(UNTER, LEAVES), Card(UNTER, HEARTS), Card(UNTER, LEAVES),
                 Card(ACE, game_mode.suit), Card(TEN, game_mode.suit), Card(KING, game_mode.suit),
                 Card(NINE, game_mode.suit), Card(EIGHT, game_mode.suit), Card(SEVEN, game_mode.suit)]
+
+
+def sorted_hand(hand: List[Card], game_mode: Optional[GameMode]):
+    if game_mode is None:
+        trumpcards = trump_cards(GameMode(PARTNER_MODE, BELLS))
+    else:
+        trumpcards = trump_cards(game_mode)
+    trumps_in_hand = [trump for trump in trumpcards if trump in hand]
+    bells = [Card(rank, BELLS) for rank in ALL_RANKS if (rank, BELLS) in hand and (rank, BELLS) not in trumpcards]
+    hearts = [Card(rank, HEARTS) for rank in ALL_RANKS if
+              (rank, HEARTS) in hand and (rank, HEARTS) not in trumpcards]
+    leaves = [Card(rank, LEAVES) for rank in ALL_RANKS if
+              (rank, LEAVES) in hand and (rank, LEAVES) not in trumpcards]
+    acorns = [Card(rank, ACORNS) for rank in ALL_RANKS if
+              (rank, ACORNS) in hand and (rank, ACORNS) not in trumpcards]
+    sorted_hand = trumps_in_hand + acorns + leaves + hearts + bells
+    return sorted_hand
--- schafkopf/src/game/player_hand.py
+++ schafkopf/src/game/player_hand.py
@@ -1,8 +1,30 @@
-from typing import List
+from typing import List, Optional
 
 from schafkopf.src.game.cards import Card
+from schafkopf.src.game.game_mode import GameMode
+from schafkopf.src.game.game_type import PARTNER_MODE
+from schafkopf.src.game.helper_functions import trump_cards
+from schafkopf.src.game.ranks import ALL_RANKS
+from schafkopf.src.game.suits import BELLS, HEARTS, LEAVES, ACORNS
 
 
 class PlayerHand(List[Card]):
-    def __init__(self, *args, **kwargs):
-        super(PlayerHand, self).__init__(args[0])
+    def __init__(self, cards):
+        super(PlayerHand, self).__init__(cards)
+
+    def sorted_hand(self, game_mode: Optional[GameMode]):
+        if game_mode is None:
+            trumpcards = trump_cards(GameMode(PARTNER_MODE, BELLS))
+        else:
+            trumpcards = trump_cards(game_mode)
+        trumps_in_hand = [trump for trump in trumpcards if trump in self]
+        bells = [Card(rank, BELLS) for rank in ALL_RANKS if (rank, BELLS) in self and (rank, BELLS) not in trumpcards]
+        hearts = [Card(rank, HEARTS) for rank in ALL_RANKS if
+                  (rank, HEARTS) in self and (rank, HEARTS) not in trumpcards]
+        leaves = [Card(rank, LEAVES) for rank in ALL_RANKS if
+                  (rank, LEAVES) in self and (rank, LEAVES) not in trumpcards]
+        acorns = [Card(rank, ACORNS) for rank in ALL_RANKS if
+                  (rank, ACORNS) in self and (rank, ACORNS) not in trumpcards]
+        sorted_hand = trumps_in_hand + acorns + leaves + hearts + bells
+        return sorted_hand
+
